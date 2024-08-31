7ae04cb9cfca0e92949e72a4ddd0db007fa9f621
MaksimObozniy <125642497+MaksimObozniy@users.noreply.github.com>
2024-08-31 19:01:44 +0400

    Update storage_information_view.py

--- django-orm-watching-storage/datacenter/storage_information_view.py
+++ django-orm-watching-storage/datacenter/storage_information_view.py
@@ -1,31 +1,27 @@
-from datacenter.models import Passcard
-from datacenter.models import Visit
 from django.shortcuts import render
 from django.utils.timezone import localtime
+from datacenter.models import Visit
 
-
-def duration_at(visits):
+def duration_at(visit):
     now = localtime()
-    then = visits.entered_at
+    then = visit.entered_at
     delta = now - then
-    result = (str(delta).split('.')[0])
+    result = str(delta).split('.')[0]
     return result
 
-
 def storage_information_view(request):
-    visit = Visit.objects.all()
-    for visits in visit:
-        if visits.leaved_at == None:
-            owner = visits.passcard
-            
-    non_closed_visits = [
-        {
-            'who_entered': owner.owner_name,
-            'entered_at': visits.entered_at,
-            'duration': duration_at(visits),
-        }
-    ]
+    non_closed_visits = []
+
+    visits = Visit.objects.filter(leaved_at__isnull=True)
+    for visit in visits:
+        non_closed_visits.append({
+            'who_entered': visit.passcard.owner_name,
+            'entered_at': visit.entered_at,
+            'duration': duration_at(visit),
+        })
+
     context = {
         'non_closed_visits': non_closed_visits, 
     }
     return render(request, 'storage_information.html', context)
+    
