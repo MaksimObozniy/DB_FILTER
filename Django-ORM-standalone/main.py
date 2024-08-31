0145b15b4a0674ec5fbd5ed88ac4546273678563
MaksimObozniy <125642497+MaksimObozniy@users.noreply.github.com>
2024-08-31 14:01:13 +0400

    Create main.py

---
+++ Django-ORM-standalone/main.py
@@ -0,0 +1,36 @@
+import os
+import django
+import datetime
+import math
+from django.utils.timezone import localtime, now
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+django.setup()
+
+from datacenter.models import Passcard, Visit  # noqa: E402
+
+def find_suspicious_visits(visits, min_duration_minutes=1000): #передаем список посетитетелей и время, которое ищем, будь то 10 минт или час
+    suspicious_visits = [] #создаем пустой список куда будем их добавлять
+    for visit in visits:
+        if visit.leaved_at is None: #делаем проверку на то, есть ли время выхода
+            leaved_at = now() #если нет, то он сейчас там и записываем туда нынешнее время
+        else:
+            leaved_at = visit.leaved_at #обратная ситуация соответственно есть время выхода
+        delta = leaved_at - visit.entered_at
+        duration_in_minutes = delta.total_seconds() / 60 #переводим в удобный формат
+        if duration_in_minutes > min_duration_minutes:  #проверяем, что время не больше 60 минут, если больше то добавляем в список
+            suspicious_visits.append(visit)
+    return suspicious_visits
+    
+    
+
+if __name__ == '__main__':
+    # Программируем здесь
+    
+    passcards = Passcard.objects.all()
+    for passcards in passcards:
+        visits = Visit.objects.filter(passcard=passcards)
+        suspicious_visits = find_suspicious_visits(visits)
+        first_suspect = suspicious_visits
+        print(first_suspect)
+        
